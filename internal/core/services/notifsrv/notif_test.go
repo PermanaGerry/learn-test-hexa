444d358d3fa38d649d4d2136eaa9f5c57cfad0d3
PermanaGerry <permana.gerry@gmail.com>
2023-02-16 11:00:16 +0700

    add unit test service

--- internal/core/services/notifsrv/notif_test.go
+++ internal/core/services/notifsrv/notif_test.go
@@ -1,4 +1,4 @@
-package notifhdl_test
+package notifsrv_test
 
 import (
 	// "fmt"
@@ -108,6 +108,82 @@ func TestSend(t *testing.T) {
 
 		result, err := service.Send(domain.NewNotif(tt.args.id, tt.args.message, tt.args.tipe))
 
+		if tt.want.err != nil && err != nil {
+			assert.Equal(tt.want.err.Error(), err.Error())
+		}
+
+		assert.Equal(tt.want.result, result, "they should be equal")
+	}
+}
+
+func TestList(t *testing.T) {
+	assert := assert.New(t)
+
+	list := domain.ShowNotif{
+		Lists: []domain.PushNotif{
+			{
+				ID:      "test-test-test-1",
+				Message: "test",
+				Type:    1,
+			},
+			{
+				ID:      "test-test-test-2",
+				Message: "test 2",
+				Type:    2,
+			},
+		},
+	}
+
+	type want struct {
+		result domain.ShowNotif
+		err    error
+	}
+
+	test := []struct {
+		name  string
+		want  want
+		mocks func(m mocks)
+	}{
+		{
+			name: "[positif] Should show list successfully",
+			want: want{result: list},
+			mocks: func(m mocks) {
+				m.notifRepository.EXPECT().
+					Show().
+					Return(list, nil)
+			},
+		},
+		{
+			name: "[negatif] Should return error - error get redis",
+			want: want{err: errors.New("error get redis")},
+			mocks: func(m mocks) {
+				m.notifRepository.EXPECT().
+					Show().
+					Return(domain.ShowNotif{}, errors.New("error get redis"))
+			},
+		},
+		{
+			name: "[negatif] Should return error - error get redis",
+			want: want{err: errors.New("error unmarshall data redis")},
+			mocks: func(m mocks) {
+				m.notifRepository.EXPECT().
+					Show().
+					Return(domain.ShowNotif{}, errors.New("error unmarshall data redis"))
+			},
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+
+		m := mocks{
+			notifRepository: mockups.NewMockNotifRepository(gomock.NewController(t)),
+		}
+
+		tt.mocks(m)
+		service := notifsrv.New(m.notifRepository)
+
+		result, err := service.List()
 		if tt.want.err != nil && err != nil {
 			assert.Equal(tt.want.err.Error(), err.Error())
 		}
